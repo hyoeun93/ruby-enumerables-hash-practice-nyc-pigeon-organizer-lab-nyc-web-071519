f89c8d6363723693e85a32e4646ca874fc6c6a32
HyoEun Lee <hyoeun93@gmail.com>
2019-07-05 02:26:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,7 +18,7 @@ def nyc_pigeon_organizer(data)
       items.each do |feature, array|
         array.each do |name|
           pigeon_hash[name][attribute] << feature.to_s
-          if name === nombre 
+          if name == nombre 
         end 
       end 
     end 
