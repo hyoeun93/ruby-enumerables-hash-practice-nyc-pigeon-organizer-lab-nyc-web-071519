24f463f86ea6448021cb30b563eb320b045a4812
HyoEun Lee <hyoeun93@gmail.com>
2019-07-05 02:42:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,27 +1,21 @@
 def nyc_pigeon_organizer(data)
-  names = []
-  pigeon_hash = {} 
-  
-  data.each do |attribute, items|
-    items.each do |feature, array|
+  new_hash = {}
+  data.each do |property, hash|
+    hash.each do |attribute, array|
       array.each do |name|
-        names << name if !names.include?(name)
-      end 
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name].has_key?(property)
+          new_hash[name][property] = []
+        end
+
+        if !new_hash[name][property].include?(attribute)
+          new_hash[name][property] << attribute.to_s
+        end
+      end
     end
-  end 
-  
-  names.each do |nombre|
-    pigeon_hash[nombre] = Hash.new{|k, v| k[v] = []}
-    data.each do |attribute, items|
-      pigeon_hash[nombre][attribute]
-      
-      items.each do |feature, array|
-        array.each do |name|
-          pigeon_hash[name][attribute] << feature.to_s
-          if name == nombre 
-        end 
-      end 
-    end 
-  end 
-  pigeon_hash
+  end
+  new_hash
 end
